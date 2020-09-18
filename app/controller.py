00f9e87e29eeb4f5cab9c109fb2919be778c25de
CodeClan Student <codeclanstudent@codeclans-mbp-5.home>
2020-09-18 18:57:28 +0100

    second commit

--- app/controller.py
+++ app/controller.py
@@ -2,47 +2,16 @@ from app import app
 from flask import render_template, request
 from app.models.players_class import *
 from app.models.players import *
+import random
 
 
+# CREATES HOME PAGE
 @app.route("/")
 def home():
     return render_template("index.html", title="Home", players=players)
 
-import random
 
-while True:
-    print("choose your weapon")
-    player = input()
-    player = player.lower() 
-    print("my choice is ", player)
-
-    choices = ["rock", "paper", "scissors"]
-
-    computer_choice = random.choice(choices)
-    print("player 2's choice is", computer_choice)
-
-    if player in choices:
-        if player == computer_choice:
-             print("tie")
-        if player == "rock":
-            if computer_choice == "paper":
-                print ("loser")
-            elif computer_choice == "scissors":
-                print ("winnner")
-
-        if player == "paper":
-            if computer_choice == "rock":
-                print ("winner")
-            elif computer_choice == "scissors":
-                print ("loser")
-
-        if player == "scissors":
-            if computer_choice == "rock":
-                print ("loser")
-            elif computer_choice == "paper":
-                print("winner")
-
-    else:
-        print("choose again weapon not avaiable")
-
-    print("")
+#  CREATES A NEW SUBMIT PAGE LINKED IN WITH THE SUBMIT BUTTON 
+@app.route("/submit")
+def submit():
+    return render_template("submit.html", title="Move", players=players)
--- app/models/players.py
+++ app/models/players.py
@@ -1,6 +1,54 @@
 from app.models.players_class import *
+import random
 
-player1 = Players("Steve", "rock")
-player2 = Players("Rob", "scissors")
+# player1 = Players("Steve", "rock")
+# player2 = Players("Rob", "scissors")
 
-players = [player1, player2]
+# players = [player1, player2]
+
+#  FUNCTION TO CREATE A RANDOM COMPUTER SELECTION
+def computer_move():
+    choice = ["rock", "paper", "scissors"]
+    return choice[random.randint(0,2)]
+
+
+
+# FULL RUNNING RPS GAME FOR PYTHON BELOW
+
+
+# while True:
+# print("choose your weapon")
+# player = input()
+# player = player.lower() 
+# print("my choice is ", player)
+
+# choices = ["rock", "paper", "scissors"]
+
+# computer_choice = random.choice(choices)
+# print("player 2's choice is", computer_choice)
+
+# if player in choices:
+#     if player == computer_choice:
+#          print("tie")
+#     if player == "rock":
+#         if computer_choice == "paper":
+#             print ("loser")
+#     elif computer_choice == "scissors":
+#         print ("winnner")
+
+#     if player == "paper":
+#         if computer_choice == "rock":
+#             print ("winner")
+#         elif computer_choice == "scissors":
+#             print ("loser")
+
+#     if player == "scissors":
+#         if computer_choice == "rock":
+#             print ("loser")
+#         elif computer_choice == "paper":
+#             print("winner")
+
+# else:
+#     print("choose again weapon not avaiable")
+
+# print("")
